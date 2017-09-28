47575cb84a5beea445acbb25b4c21e1837d7b75e
LilianaIvonne <31258753+LilianaIvonne@users.noreply.github.com>
2017-09-28 16:50:36 -0500

    Update validar.php

--- web2/validar.php
+++ web2/validar.php
@@ -7,10 +7,10 @@ $sql2=mysqli_query($link,"SELECT * FROM login WHERE username='$username'");
 if($fd=mysqli_fetch_array($sql2)){
 	if($pass == $fd['passadmin']){
 		echo '<script>alert("Bienvenido administrador")</script>';
-		echo '<script>location.href=Menu.php</script>';
+		echo "<script>location.href='Menu.php'</script>";
 	}
 	else
 		echo '<script>alert("Contraseña incorrecta")</script>';
-		echo '<script>location.href=login.php</script>';
+		echo "<script>location.href='login.php'</script>";
 }
 ?>
